139659ab3c90e628a54ab9980415f6ed6676846a
Mudit Gupta <muditgupta.jaipur@gmail.com>
2023-11-02 05:22:46 -0400

    updated port

--- launch/lidar.launch.py
+++ launch/lidar.launch.py
@@ -10,7 +10,7 @@ def generate_launch_description():
                 package="xv_11_driver",
                 executable="xv_11_driver",
                 output="screen",
-                parameters=[{"serial_port": "/dev/ttyACM0", "frame_id": "laser_frame"}],
+                parameters=[{"port": "/dev/ttyACM0", "frame_id": "laser_frame"}],
             )
         ]
     )
