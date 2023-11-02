f5205258b426841af16a582983342b218cb2b3f3
Mudit Gupta <muditgupta.jaipur@gmail.com>
2023-11-02 03:47:39 -0400

    renamed colcon build space added lidar launch file

--- launch/lidar.launch.py
+++ launch/lidar.launch.py
@@ -2,17 +2,15 @@ import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
-
-    return LaunchDescription([
-
-        Node(
-            package='xv_11_driver',
-            executable='xv_11_driver',
-            output='screen',
-            parameters=[{
-                'serial_port': '/dev/ttyACM0',
-                'frame_id': 'laser_frame'
-            }]
-        )
-    ])
+    return LaunchDescription(
+        [
+            Node(
+                package="xv_11_driver",
+                executable="xv_11_driver",
+                output="screen",
+                parameters=[{"serial_port": "/dev/ttyACM0", "frame_id": "laser_frame"}],
+            )
+        ]
+    )
