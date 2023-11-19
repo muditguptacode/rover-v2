a1f0c1155e82df65742877de5ee1400eb473e807
Mudit Gupta <muditgupta.jaipur@gmail.com>
2023-11-19 16:32:43 -0500

    minor

--- misc/roboclaw_python/testing_comms_speed.py
+++ misc/roboclaw_python/testing_comms_speed.py
@@ -13,36 +13,24 @@ PPR = int(4096/2)
 BUFFER = 0
 time.sleep(1)
 
-for i in range(0, 1):
-    roboclaw.SpeedDistanceM1M2(rightAddress, MAX_SPEED_LIMIT, PPR, MAX_SPEED_LIMIT, PPR, BUFFER)
-    roboclaw.SpeedDistanceM1M2(leftAddress, MAX_SPEED_LIMIT, PPR, MAX_SPEED_LIMIT, PPR, BUFFER)
-    roboclaw.SpeedDistanceM1M2(leftAddress, 0, 0, 0, 0, 0)
-    roboclaw.SpeedDistanceM1M2(rightAddress, 0, 0, 0, 0, 0)
-    roboclaw.SpeedDistanceM1M2(leftAddress, -MAX_SPEED_LIMIT, PPR, -MAX_SPEED_LIMIT, PPR, BUFFER)
-    roboclaw.SpeedDistanceM1M2(rightAddress, -MAX_SPEED_LIMIT, PPR, -MAX_SPEED_LIMIT, PPR, BUFFER)
-    roboclaw.SpeedDistanceM1M2(leftAddress, 0, 0, 0, 0, 0)
-    roboclaw.SpeedDistanceM1M2(rightAddress, 0, 0, 0, 0, 0)
-    # buffLeft = roboclaw.ReadBuffers(leftAddress)
-    # buffRight = roboclaw.ReadBuffers(rightAddress)
-    while True:
-        time.sleep(0.1)
-        lbf, lbm1, lbm2 = roboclaw.ReadBuffers(leftAddress)
-        rbf, rbm1, rbm2 = roboclaw.ReadBuffers(rightAddress)
-        print("Buffers: ", lbm1, lbm2, rbm1, rbm2)
-        if lbm1 == 128 and lbm2 == 128 and rbm1 == 128 and rbm2 == 128:
-            break
-        # leftM1Current = roboclaw.ReadM1MaxCurrent(leftAddress)
-        # leftM2Current = roboclaw.ReadM2MaxCurrent(leftAddress)
-        # rightM1Current = roboclaw.ReadM1MaxCurrent(rightAddress)
-        # rightM2Current = roboclaw.ReadM2MaxCurrent(rightAddress)
-        # print("Motor currents: ", leftM1Current, rightM1Current)
-        # print("Motor currents: ", leftM2Current, rightM2Current)
-        # print("RM2 Max Current: ", roboclaw.ReadM2MaxCurrent(rightAddress))
-        # print("Right Cur Current: ", roboclaw.ReadCurrents(rightAddress))
-        # print("Left Cur Current: ", roboclaw.ReadCurrents(leftAddress))
-    print("ALL DONE!")
+roboclaw.SpeedM1M2(rightAddress, MAX_SPEED_LIMIT, MAX_SPEED_LIMIT)
+# roboclaw.SpeedDistanceM1M2(leftAddress, MAX_SPEED_LIMIT, PPR, MAX_SPEED_LIMIT, PPR, BUFFER)
+# roboclaw.SpeedDistanceM1M2(leftAddress, 0, 0, 0, 0, 0)
+# roboclaw.SpeedDistanceM1M2(rightAddress, 0, 0, 0, 0, 0)
+# roboclaw.SpeedDistanceM1M2(leftAddress, -MAX_SPEED_LIMIT, PPR, -MAX_SPEED_LIMIT, PPR, BUFFER)
+# roboclaw.SpeedDistanceM1M2(rightAddress, -MAX_SPEED_LIMIT, PPR, -MAX_SPEED_LIMIT, PPR, BUFFER)
+# roboclaw.SpeedDistanceM1M2(leftAddress, 0, 0, 0, 0, 0)
+# roboclaw.SpeedDistanceM1M2(rightAddress, 0, 0, 0, 0, 0)
+while True:
+    time.sleep(0.1)
+    lbf, lbm1, lbm2 = roboclaw.ReadBuffers(leftAddress)
+    rbf, rbm1, rbm2 = roboclaw.ReadBuffers(rightAddress)
+    print("Buffers: ", lbm1, lbm2, rbm1, rbm2)
+    if lbm1 == 128 and lbm2 == 128 and rbm1 == 128 and rbm2 == 128:
+        break
+print("ALL DONE!")
 
-    while True:
-        print(time.localtime(), "Main Battery Left Voltage: ", roboclaw.ReadMainBatteryVoltage(leftAddress))
-        print(time.localtime(),"Main Battery Right Voltage: ", roboclaw.ReadMainBatteryVoltage(rightAddress))
-        time.sleep(1)
+while True:
+    print(time.localtime(), "Main Battery Left Voltage: ", roboclaw.ReadMainBatteryVoltage(leftAddress))
+    print(time.localtime(),"Main Battery Right Voltage: ", roboclaw.ReadMainBatteryVoltage(rightAddress))
+    time.sleep(1)
