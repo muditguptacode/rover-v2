8d4144665be120724859d58745a0176d85a08e1f
Mudit Gutpa <muditgupta.jaipur@gmail.com>
2023-11-29 11:34:58 -0500

    minor

--- scripts/launch-browser.sh
+++ scripts/launch-browser.sh
@@ -1,2 +1,2 @@
-docker exec -w "/rover-v2/webui/" ros2_rover bash -c "pkill http-server && http-server ."
+docker exec -w "/rover-v2/webui/" -d ros2_rover bash -c "pkill http-server && http-server ."
 DISPLAY=:0 chromium-browser -kiosk --incognito http://10.10.20.152:8080
